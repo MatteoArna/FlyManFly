d07285ef35ecd55275760ab463a094d4f41df589
MatteoArna <matteo.arena711@gmail.com>
2022-09-24 11:01:03 +0200

    feat: :sparkles: Enemies implementation
    The implementation of the enemies is now complete

--- src/Game.java
+++ src/Game.java
@@ -7,9 +7,13 @@ public class Game {
 
         GameBoard gb = new GameBoard("FLY MAN, FLY!", "Matteo & Nathan");
         
-        //gb.welcomeScreen(5000);
-        //gb.tutorialScreen(input);
-
+        try {
+            gb.welcomeScreen(5000);
+            gb.tutorialScreen(input);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+        
         try {
             gb.play(input);
         } catch (IOException | InterruptedException e) {
